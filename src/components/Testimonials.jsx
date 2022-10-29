f245204609cbaa03bbcaf08c7dc9c91d95658292
taiseen <taiseen.cse@gmail.com>
2022-10-29 16:18:35 +0600

    testimonials section working...

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
-}
+const Testimonials = () => (
+  <div>
+
+  </div>
+)
 
 export default Testimonials
